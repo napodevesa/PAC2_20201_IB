3f9abaa8c860f64473b6337f7ac79d63c6611bb5
ndev <napodevesa@gmail.com>
2020-10-05 22:39:05 +0200

    ejercicio 2 finish

--- PAC2Ex2/src/edu/uoc/pac2/Station.java
+++ PAC2Ex2/src/edu/uoc/pac2/Station.java
@@ -2,6 +2,7 @@
 package edu.uoc.pac2;
 
 
+
 public class Station {
 	private int id;
 	private static int nextId = 0;
@@ -13,15 +14,27 @@ public class Station {
 	
 	
 
-	public Station( String name, String street, double latitude, double longitude, int capacity) {
+	public Station( String name, String street, double latitude, double longitude, int capacity) throws Exception {
 		// TODO Auto-generated constructor stub
 		
-		setId(nextId++);
+		//setId(nextId++);
 		setName(name);
 		setStreet(street);
 		setLatitude(latitude);
 		setLongitude(longitude);
 		setCapacity(capacity);
+		
+		setNextId(nextId);
+		
+		
+		try  
+	      {  
+			setId(nextId++);
+	      }  
+	      catch(Exception e)  
+	      {  
+	    	  setId(id);;
+	      }  
 		
 		/*
 		this.id= id;
@@ -30,24 +43,30 @@ public class Station {
 		this.latitude =  latitude;
 		this.longitude = longitude;
 		this.capacity = capacity;
-		
 		setId(nextId++);
 		//nextId++;
 	
 	*/
 		
+		
+		
 	}
 
 
 
-	public Station() {
+	public Station() throws Exception {
 		// TODO Auto-generated constructor stub
+		
 		setId(nextId++);
 		setName(name);
 		setStreet(street);
 		setLatitude(latitude);
 		setLongitude(longitude);
 		setCapacity(capacity);
+		
+		setNextId(nextId);
+		
+		
 	}
 
 
@@ -59,6 +78,9 @@ public class Station {
 	public void setId(int id) {
 		//this.id = nextId++ ;
 		this.id = id ;
+		
+		
+		
 	}
 	
 	
@@ -80,15 +102,15 @@ public class Station {
 	}
 	
 	
-	public void setLatitude(double latitude) {
+	public void setLatitude(double latitude) throws Exception {
 		
-		if (latitude <= 90 && latitude >= -90 ) { 
+		if (latitude > 90 || latitude < -90 ) { 
+			throw new Exception ("[ERROR] Station's latitude "
+					+ "must be in range [-90,+90]");
 	
-			this.latitude = (float) latitude;
-		} else {
-				System.out.println("[ERROR] Station's latitude "
-						+ "must be in range [-90,+90]");
 		}
+		
+		this.latitude = (float) latitude;
 			
 	}
 	
@@ -98,13 +120,15 @@ public class Station {
 		return (float) longitude;
 	}
 	
-	public void setLongitude(double longitude) {
-		if (longitude <= 180 && longitude >= -180 ) { 
-			this.longitude = (float) longitude;
-		}else {
-			System.out.println("[ERROR] Station's longitude must "
+	public void setLongitude(double longitude) throws Exception {
+		if (longitude > 180 || longitude < -180 ) { 
+			
+			throw new Exception ("[ERROR] Station's longitude must "
 					+ "be in range [-180,+180]");
+			
+			
 		}
+		this.longitude = (float) longitude;
 	}
 	
 	//capacity
@@ -112,13 +136,12 @@ public class Station {
 	public int getCapacity() {
 		return capacity;
 	}
-	public void setCapacity(int capacity) {
-		if (capacity > 0 ) { 
-			this.capacity = capacity;
-		}else {
-			System.out.println("[ERROR] Station's capacity must "
+	public void setCapacity(int capacity) throws Exception {
+		if (capacity <= 0 ) { 
+			throw new Exception ("[ERROR] Station's capacity must "
 					+ "be greater than 0");
 		}
+		this.capacity = capacity;
 	}
 	
 	
@@ -129,18 +152,17 @@ public class Station {
 	}
 
 
-	public void setName(String name) {
+	public void setName(String name) throws Exception{
+		
 		if ( name.length() > 50) {
-			System.out.println("[ERROR] Station's name cannot "
+			throw new Exception ("[ERROR] Station's name cannot "
 					+ "be longer than 50 characters");	
 				
-		}else {
+		}
 			
 			this.name = name;
 		}
 
-
-	}
 
 	//nextId
 	
