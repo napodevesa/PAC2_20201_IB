f33dc5bb0a553cc46d0e4826c96b5c6f729099b7
ndev <napodevesa@gmail.com>
2020-10-07 10:17:17 +0200

    1,2,3 listos

--- PAC2Ex4/src/edu/uoc/pac2/strings/PAC2Ex4.java
+++ PAC2Ex4/src/edu/uoc/pac2/strings/PAC2Ex4.java
@@ -3,15 +3,58 @@ package edu.uoc.pac2.strings;
 public class PAC2Ex4 {
 
 	public String reverseText(String text) {
-		//TODO		
+		//TODO	
+		
+		if (text == null){             
+
+		        return null;
+		}
+		
+		String reverse = new StringBuffer(text).reverse().toString();
+		return reverse;
+		
+
 	}
 	
 	public int countM(String text) {
 		//TODO
+		if (text == null){             
+
+	        return 0;
+	}
+		int count = 0;
+		char someChar = 'm';
+		char someChar2 = 'M';
+		
+		for (int i = 0; i < text.length(); i++) {
+		   
+			if (text.charAt(i) == someChar || text.charAt(i) == someChar2 ) {
+		     count++;
+		    }
+			
+		}
+		return count;
 	}
 	
 	public String capitalize(String text, int index) {		
 		//TODO
+		
+		if (text == null){             
+
+	        return null;
+		}
+		
+		if (index <= 0){             
+
+	        return text;
+		}
+		
+		if (index >= text.length()) {
+		
+			return text.toUpperCase();
+		
+		}  
+		return text;
 	}
 
 }
